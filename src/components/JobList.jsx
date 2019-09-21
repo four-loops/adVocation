b4ff73e89b15ae6904fc0e4919998552810d2f69
Julia Kim <kimjulia313@gmail.com>
2019-09-21 17:55:44 -0500

    Render search bar to page

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Form, Button } from 'react-bootstrap';
 import AvailableJob from './AvailableJob.jsx';
 
 const JobList = (props) => {
   return (
     <React.Fragment>
       <h3>Available Jobs</h3>
+      <Form id="seeker_jobForm">
+        <Form.Group controlId="formSearch">
+          <Form.Label>Search Bar</Form.Label>
+          <Form.Control type="text" placeholder="Search"></Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit"></Button>
+      </Form>
       <ListGroup>
         {props.location.props.jobs.map((job) => {
           return (
