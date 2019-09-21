7f3b08a24013a22f1c917488d5ed6ec9db53f7d1
Arohan Dutt <arohandutt@gmail.com>
2019-09-21 14:12:20 -0500

    Added refresh routing

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.use(express.static('dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.listen(port, () => console.log(`listening from port: ${port}`));
