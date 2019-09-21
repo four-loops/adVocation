00da1bf2bf49bea02ae6202e8e7767d93cd43789
Arohan Dutt <arohandutt@gmail.com>
2019-09-21 13:49:06 -0500

    Add header and padding

--- src/components/candidateList.jsx
+++ src/components/candidateList.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class ViewCandidates extends React.Component {
 
   constructor(props) {
     super(props)
-    this.property = 'init'
-  
+
+    this.state = {
+      candidates: [],
+      shortList: []
+    }
+
   }
 
 
   render() {
     return(
-      <Container>
-        <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
+      <div className='candidateList_body'>
+        <h1>Candidates</h1>
+        <Container>
+          <Row>
+            <Col className='identifier'>1 of 2</Col>
+            <Col className='identifier'>2 of 2</Col>
+          </Row>
+          <Row>
+            <Col className='identifier'>1 of 3</Col>
+            <Col className='identifier'>2 of 3</Col>
+            <Col className='identifier'>3 of 3</Col>
+          </Row>
+        </Container>
+      </div>
     )
   }
 }
