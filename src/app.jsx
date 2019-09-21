3ba0467dab77c14b09bd43d94118f1d3792f4c70
Julia Kim <kimjulia313@gmail.com>
2019-09-21 10:44:09 -0500

    Update description on package.json

--- src/app.jsx
+++ src/app.jsx
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div class="jumbotron">
+        <div className="jumbotron">
         <h1 class="display-4">Amazing React, Bootstrap and Webpack</h1>
         <p class="lead">Created with love</p>
         <hr class="my-4"/>
