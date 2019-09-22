9d2ab217a24aaa0737e0ca3cae3ecc8744067fea
Arohan Dutt <arohandutt@gmail.com>
2019-09-21 21:07:55 -0500

    Add icon package, hide buttons on job view

--- src/components/GiverLanding.jsx
+++ src/components/GiverLanding.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ListGroup, Button } from 'react-bootstrap';
+import Octicon, {iconsByName} from '@primer/octicons-react'
 
 class GiverLanding extends React.Component {
 
@@ -8,10 +9,17 @@ class GiverLanding extends React.Component {
     super(props)
 
     this.state = {
+      giverCompany: 'Galvanize',
       jobs: [
         {job_id: 13213, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
         {job_id: 13203, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
         {job_id: 13123, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+        {job_id: 23434, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+        {job_id: 14343, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+        {job_id: 13423, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+        {job_id: 14723, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+        {job_id: 13783, company_id: 'Galvanize', title: 'Software Engineer', city: 'Austin', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'},
+
       ],
       renderJobView: false,
       currentJob: {job_id: 1, company_id: 'Placeholder', title: 'Place Holder', city: 'Placeholder', 'state': 'TX', snippet: 'Lorem ipsum dolor sit amet', url: 'http://www.google.com'}
@@ -41,7 +49,7 @@ class GiverLanding extends React.Component {
     return(
       <div id='giverLanding_Body'>
         <h2>Hello, Name!</h2>
-        <h4>Here are jobs listed at COMPANY that you can refer for:</h4>
+        <h4>Here are jobs listed at {this.state.giverCompany} that you can refer for:</h4>
         <div className='giverLanding_ListContainer'>
           {
             this.state.renderJobView 
@@ -50,10 +58,13 @@ class GiverLanding extends React.Component {
               <h2>{this.state.currentJob.title}</h2>
               <p>{this.state.currentJob.snippet}</p>
               <div className='giverLanding_JobButtonWrapper'>
-                <Button className='giverLanding_JobButton'>View Full Job</Button>
-                <Button className='giverLanding_JobButton'
-                        onClick={() => this.renderJob()}
-                        >Back to Job List</Button>
+                <a href={this.state.currentJob.url} target='blank'>
+                  <Button className='giverLanding_JobButton'>
+                    <span>View Full Job </span>
+                    <Octicon icon={iconsByName['link-external']}/>
+                  </Button>
+                </a>
+                <Button className='giverLanding_JobButton'> Refer for this position</Button>
               </div>
             </div> 
             :
@@ -71,10 +82,16 @@ class GiverLanding extends React.Component {
             </ListGroup>
           }
         </div>
-        <div className='giverLanding_ButtonHolder'>
-          <Button className='giverLanding_Button'>Preferences</Button>
-          <Button className='giverLanding_Button'>Add Job</Button>
-        </div>
+        {
+          this.state.renderJobView 
+          ? 
+          <Button className='giverLanding_JobButton' onClick={() => this.renderJob()} >Back to Job List</Button> 
+          :
+          <div className='giverLanding_ButtonHolder'>
+            <Button className='giverLanding_Button'>Preferences</Button>
+            <Button className='giverLanding_Button'>Add Job</Button>
+          </div>
+        }
       </div>
     )
   }
