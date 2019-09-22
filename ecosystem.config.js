1f2f3e82106e238dcac1f7558c1e1cb307facca6
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-22 12:50:53 -0500

    Update scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ref: 'origin/master',
         repo: 'https://github.com/four-loops/adVocation.git',
         path: '/home/ubuntu/adVocation',
-        'post-deploy': 'npm install && npm start && cp ~/adVocation/current/bootstrap-theme.min.css ~/adVocation/current/node_modules/bootstrap/dist/css && npm run compile && pm2 startOrRestart ecosystem.config.js'
+        'post-deploy': 'npm install --ignore-scripts && npm start && cp ~/adVocation/current/bootstrap-theme.min.css ~/adVocation/current/node_modules/bootstrap/dist/css && npm run compile && pm2 startOrRestart ecosystem.config.js'
       }
     }
   }
